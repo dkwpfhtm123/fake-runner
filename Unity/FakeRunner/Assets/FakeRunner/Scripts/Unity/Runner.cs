c47d42e97bebd0792e497d3a46d53ef3826009be
류동균 <dkxmzkak123@gmail.com>
2016-11-21 15:54:38 +0900

    숙제= 애니메이션 자연스럽게 이미지 원점 발밑으로 맵 타일 생성하는 스크립트. deactive active 활용

---
+++ Unity/FakeRunner/Assets/FakeRunner/Scripts/Unity/Runner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fake.FakeRunner.Unity
+{
+    public delegate void RunnerEventHandler(Runner sender);
+
+    public enum RunnerState
+    {
+        Idle,
+        Running,
+        Jump,
+    }
+
+    public class Runner : MonoBehaviour
+    {
+        private RunnerState state;
+        private Transform transformCache;
+        private float maxSpeed;
+        private float speed;
+
+        public event RunnerEventHandler PositionChanged;
+
+        private void Awake()
+        {
+            state = RunnerState.Running;
+            transformCache = GetComponent<Transform>();
+            maxSpeed = 5.0f;
+            speed = maxSpeed;
+        }
+
+        private void Update()
+        {
+            switch (state)
+            {
+                case RunnerState.Running:
+                    OnRunning();
+                    break;
+            }
+        }
+
+        private void OnRunning()
+        {
+            transformCache.localPosition += Vector3.right * speed * Time.deltaTime;
+
+            if (PositionChanged != null)
+                PositionChanged(this);
+        }
+    }
+}
---
+++ Unity/FakeRunner/Assets/FakeRunner/Scripts/Unity/RunnerTracker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Fake.FakeRunner.Unity
+{
+    public class RunnerTracker : MonoBehaviour
+    {
+        public Runner runner;
+        public Vector3 offset;
+        private Transform transformCache;
+        private Transform runnerTransformCache;
+
+        private void Awake()
+        {
+            transformCache = GetComponent<Transform>();
+
+            if (runner != null)
+            {
+                runnerTransformCache = runner.GetComponent<Transform>();
+                runner.PositionChanged += OnRunnerPositionChanged;
+            }
+        }
+
+        private void OnRunnerPositionChanged(Runner sender)
+        {
+            if (runner == sender)
+                transformCache.localPosition = runnerTransformCache.localPosition + offset;
+        }
+    }
+}
